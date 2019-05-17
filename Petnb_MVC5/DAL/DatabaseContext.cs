322a60401918109a7410751a11fe4ee045780a30
adrian <lupescua@gmail.com>
2019-05-17 08:58:14 +0200

    not sure what i push, but i push!

--- Petnb_MVC5/DAL/DatabaseContext.cs
+++ Petnb_MVC5/DAL/DatabaseContext.cs
@@ -8,9 +8,13 @@ using System.Data.Entity.ModelConfiguration.Conventions;using Petnb_MVC5.Models
 namespace Petnb_MVC5.DAL
 {
     public class DatabaseContext : DbContext    {
-        public PetContext() : base(PetContext")
+        public DatabaseContext() : base("DatabaseContext")
         {
-        }
+            Database.SetInitializer<DatabaseContext>(null); // Remove default initializer
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }   
+
         public DbSet<OwnerUser> OwnerUser { get; set; }
         public DbSet<SitterUser> SitterUser { get; set; }
         public DbSet<Pet> Pet { get; set; }
@@ -18,11 +22,28 @@ namespace Petnb_MVC5.DAL
         public DbSet<PetSitterOffer> PetSitterOffer { get; set; }
         public DbSet<Review> Review { get; set; }
 
+
+        public static DatabaseContext Create()
+        {
+            return new DatabaseContext();
+        }
+
+      
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
 
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        }
 
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // IMPORTANT: we are mapping the entity User to the same table as the entity ApplicationUser
+            modelBuilder.Entity<OwnerUser>().ToTable("OwnerUser");
+            modelBuilder.Entity<SitterUser>().ToTable("SitterUser");
+       
+        }
     }
+
 }
